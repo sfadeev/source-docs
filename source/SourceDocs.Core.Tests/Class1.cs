8ba4a120e9c52527460a8a018dd6e1fcf1316bc7
Sergey Fadeev <fadeev@gmail.com>
2014-11-18 00:36:16 +0300

    added LibGit2Sharp reference and repo clone sample

---
+++ source/SourceDocs.Core.Tests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LibGit2Sharp;
+using NUnit.Framework;
+
+namespace SourceDocs.Core.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            var clonedRepoPath = Repository.Clone("https://github.com/sfadeev/source-docs.git", "./temp-repo/");
+
+            using (var repo = new Repository(clonedRepoPath))
+            {
+                foreach (var item in repo.RetrieveStatus())
+                {
+                    Console.WriteLine(item.FilePath);
+                }
+            }
+        }
+    }
+}
